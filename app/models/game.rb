b9939c3e213ed723b60645c6db3f347db59b01bf
Nicholas Galante <nicholasgalante94@gmail.com>
2023-03-09 15:58:21 -0500

    commit tree a3326569cd4ec17df1bdf69ebb4a22a34eee8aa3

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,8 @@
 class Game < ActiveRecord::Base
-  
+  has_many :reviews
+
+  # Active Record's functionality for:
+  # def reviews
+  #   Review.where(game_id: self.id)
+  # end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ActiveRecord::Base
-  
+  belongs_to :game
+
+  # Active Record's functionality for:
+  # def game
+  #   Game.find(self.game_id)
+  # end
 end
